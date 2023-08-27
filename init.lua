5e045736c87e9addfa5850dbd1232c1abda67caa
Evan Johnson <ev.jonnson@gmail.com>
2023-08-27 11:25:55 -0600

    move CloseNotifications into separate lua file

--- init.lua
+++ init.lua
@@ -1,6 +1,1 @@
--- Close all visible notifications in Notification Center.
-hs.hotkey.bind({"ctrl", "cmd", "alt"}, ".", function()
-    hs.osascript.javascriptFromFile(
-      os.getenv("HOME") .. "/.hammerspoon/scripts/close_notifications.js"
-    )
-  end)
+local CloseNotifications = require('CloseNotifications')
