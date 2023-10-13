4ae23712caec07af9add95f5ab3edad20bba3152
Evan Johnson <ev.jonnson@gmail.com>
2023-10-13 14:57:02 -0600

    functions for snapping floating windows

--- yabai.lua
+++ yabai.lua
@@ -1,3 +1,5 @@
+local json = hs.json
+
 -- https://www.joshmedeski.com/posts/customizing-yabai-with-lua/
 
 -- Send message(s) to a running instance of yabai.
@@ -13,18 +15,44 @@ local function alt(key, commands)
   end)
 end
 
+local floatingSnaps = {
+  full = "window --grid 1:1:0:0:1:1",
+  leftHalf = "window --grid 2:2:0:0:1:2",
+  rightHalf = "window --grid 2:2:1:0:1:2",
+  topHalf = "window --grid 2:2:0:0:2:1",
+  bottomHalf = "window --grid 2:2:0:1:2:1",
+  topLeftCorner = "window --grid 2:2:0:0:1:1",
+  topRightCorner = "window --grid 2:2:1:0:1:1",
+  bottomLeftCorner = "window --grid 2:2:0:1:1:1",
+  bottomRightCorner =  "window --grid 2:2:1:1:1:1"
+}
+
 local function floatWarpRight()
-  local jsonString = hs.execute("/usr/local/bin/yabai -m query --displays --display")
-  print(jsonString)
+  local displayJsonString = hs.execute("/usr/local/bin/yabai -m query --displays --display")
+  local displayJson = json.decode(displayJsonString)
+  local windowJsonString = hs.execute("/usr/local/bin/yabai -m query --windows --window")
+  local windowJson = json.decode(windowJsonString)
+
+  if windowJson.frame.h <= displayJson.frame.h / 2 and windowJson.frame.x == 0 then
+    if windowJson.frame.y < displayJson.frame.h / 2 then
+      yabai({ "window --grid 2:2:1:0:1:1" })
+    else
+      yabai({ "window --grid 2:2:1:1:1:1" })
+    end
+    print('hey hey')
+  else
+    yabai()
+  end
 end
 
-hs.hotkey.bind({ "alt" }, "m", function()
-  floatWarpRight()
+hs.hotkey.bind({ "alt" }, "j", function()
+  snapLeftHalf()
 end)
 
--- operations on floating windows
-alt("j", { "window --grid 1:2:0:0:1:1" })
-alt("l", { "window --grid 1:2:1:0:1:1" })
+hs.hotkey.bind({ "alt" }, "l", function()
+snapRightHalf()
+end)
+
 -- alt("m", { "query --displays --display" })
 alt("p", { "window --toggle pip" })
 alt("g", { "space --toggle padding", "space --toggle gap" })
