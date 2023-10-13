2cc522cf6f0f435aed4411845e1b0adc5e692058
Evan Johnson <ev.jonnson@gmail.com>
2023-10-13 11:17:04 -0600

    basic config copied from internet

--- yabai.lua
+++ yabai.lua
@@ -1,21 +1,30 @@
 -- https://www.joshmedeski.com/posts/customizing-yabai-with-lua/
 
-local function alt(key, commands)
-    hs.hotkey.bind({ "alt" }, key, function()
-      yabai(commands)
-    end)
+-- Send message(s) to a running instance of yabai.
+local function yabai(commands)
+  for _, cmd in ipairs(commands) do
+    os.execute("/usr/local/bin/yabai -m " .. cmd)
   end
+end
 
-  -- alpha
-  alt("f", { "window --toggle zoom-fullscreen" })
-  alt("l", { "space --focus recent" })
-  alt("m", { "space --toggle mission-control" })
-  alt("p", { "window --toggle pip" })
-  alt("g", { "space --toggle padding", "space --toggle gap" })
-  alt("r", { "space --rotate 90" })
-  alt("t", { "window --toggle float", "window --grid 4:4:1:1:2:2" })
+local function alt(key, commands)
+  hs.hotkey.bind({ "alt" }, key, function()
+    yabai(commands)
+  end)
+end
 
-  -- special characters
-  alt("'", { "space --layout stack" })
-  alt(";", { "space --layout bsp" })
-  alt("tab", { "space --focus recent" })
+local function floatWarpRight()
+end
+
+-- operations on floating windows
+alt("j", { "window --grid 1:2:0:0:1:1" })
+alt("l", { "window --grid 1:2:1:0:1:1" })
+alt("m", { "space --toggle mission-control" })
+alt("p", { "window --toggle pip" })
+alt("g", { "space --toggle padding", "space --toggle gap" })
+alt("r", { "space --rotate 90" })
+alt("t", { "window --toggle float", "window --grid 4:4:1:1:2:2" })
+
+-- special characters
+alt("'", { "space --layout stack" })
+alt(";", { "space --layout bsp" })
