39db9c975db9bba7ad74cef873866339a47be17d
Evan Johnson <ev.jonnson@gmail.com>
2023-10-14 12:27:35 -0600

    working inin

--- init.lua
+++ init.lua
@@ -3,4 +3,9 @@ local Caffeine = require('Caffeine')
 local ReloadConfig = require('ReloadConfig')
 -- local ConfigAutoLoad = require('ConfigAutoLoad')
 local ModifierTapMods = require('ModifierTapMods')
+local yabai = require ('yabai')
+-- local TerminalCmdToCtrl = require('TerminalCmdToCtrl')
 -- local scratchpad = require('scratchpad')
+
+-- SPOONS
+-- hs.loadSpoon(VolumeScroll)
