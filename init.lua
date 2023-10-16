8702f314b7c49828a87758b222ce6222bfce9196
Evan Johnson <ev.jonnson@gmail.com>
2023-10-15 23:11:10 -0600

    create WindowGrid.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 local CloseNotifications = require('CloseNotifications')
 local Caffeine = require('Caffeine')
 local ReloadConfig = require('ReloadConfig')
--- local ConfigAutoLoad = require('ConfigAutoLoad')
 local ModifierTapMods = require('ModifierTapMods')
 local Hotkeys = require('Hotkeys')
-local yabai = require('yabai')
--- local ViewKeyInfo = require('ViewKeyInfo')h
+-- local WindowGrid = require('WindowGrid')
+-- local yabai = require('yabai')
+-- local ViewKeyInfo = require('ViewKeyInfo')
 -- local TerminalCmdToCtrl = require('TerminalCmdToCtrl')
 -- local scratchpad = require('scratchpad')
 
