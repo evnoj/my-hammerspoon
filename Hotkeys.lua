b062a223ab3ed6bd8696611cfa64cbcd052e98b6
Evan Johnson <ev.jonnson@gmail.com>
2023-10-17 07:25:55 -0600

    remove failed charmstone hotkey

--- Hotkeys.lua
+++ Hotkeys.lua
@@ -2,8 +2,3 @@
 hs.hotkey.bind({ "alt" }, "h", function()
     hs.application.frontmostApplication():hide()
 end)
-
--- hotkeys for charmstone
-hs.hotkey.bind({ "alt" }, "w", function()
-    hs.application.frontmostApplication():hide()
-end)
