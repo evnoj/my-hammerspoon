9abe3ff2170c068b03c4e31efa3a922f5e259c2f
Evan Johnson <ev.jonnson@gmail.com>
2023-10-21 20:17:12 -0600

    add hotkey for focus menubar

--- Hotkeys.lua
+++ Hotkeys.lua
@@ -1,4 +1,16 @@
+local eventtap = require("hs.eventtap")
+local keycodes = require("hs.keycodes")
+
 -- to use alt-h as the global macOS 'hide' command
 hs.hotkey.bind({ "alt" }, "h", function()
     hs.application.frontmostApplication():hide()
+end)
+
+-- send ctrl+f2 to focus the menubar when it is hidden
+hs.hotkey.bind({ "shift,cmd,alt" }, "m", function()
+    print("i did it")
+    eventtap.event.newKeyEvent(keycodes.map.ctrl, true):post()
+    eventtap.event.newKeyEvent("f2", true):post()
+    eventtap.event.newKeyEvent("f2", false):post()
+    eventtap.event.newKeyEvent(keycodes.map.ctrl, false):post()
 end)
