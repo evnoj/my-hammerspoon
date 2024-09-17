ed2d4aad5028fc8dfdfb083d08ff67d2856d7dbd
Evan Johnson <ev.jonnson@gmail.com>
2024-09-17 10:01:43 -0600

    set hide to hyper-/

--- Hotkeys.lua
+++ Hotkeys.lua
@@ -6,10 +6,33 @@ hs.hotkey.bind({ "ctrl, cmd, alt" }, "'", function()
     hs.window.focusedWindow():minimize()
 end)
 
--- to use hyper-shift-' as the global macOS 'hide' command
-hs.hotkey.bind({ "ctrl, cmd, alt", "shift" }, "'", function()
+-- to use hyper-/ as the global macOS 'hide' command
+hs.hotkey.bind({ "ctrl, cmd, alt" }, "/", function()
     hs.application.frontmostApplication():hide()
 end)
+
+-- failed experiment to send a string of keys, unknown why not working
+-- hs.hotkey.bind({ "ctrl, alt" }, "9", function()
+--     os.execute("sleep 5")
+--     text = '98bfMqcuXnZJ4vb6pfQrHFaD1iKBbXdApouzmfDOXTF9taapiGDprqnUU1RqiEJbmdtfmeosiniLpGhVw6hP1ruLcbRW6DJLWtyEurZicAPiGwTORcOKv3R1mfVtOxFfhCgyQitMJpXrXtwDhxzyN16H8QwgBbTHfKOyBtgDdDr3wJTuuKuyG6ufcvnTCak704dTeaUV34EQMJ6XU26NBUqsghPQAAA124A8Kfusf8TOw3W1RgVTVcCUFwMgAkPaKOcZNtNDiKNUfO9MetyqvbfriC9nGQfUF4rb1FAm'
+
+--     for i = 1, 10 do
+--     -- for i = 1, #text do
+--         local c = text:sub(i,i)
+--         print(c)
+
+--         if (c ~= string.upper(c)) then
+--             hs.eventtap.event.newKeyEvent(c, true):post()
+--             hs.eventtap.event.newKeyEvent(c, false):post()
+--         else
+--             hs.eventtap.event.newKeyEvent(hs.keycodes.map.shift, true):post()
+--             hs.eventtap.event.newKeyEvent(c, true):post()
+--             hs.eventtap.event.newKeyEvent(c, false):post()
+--             hs.eventtap.event.newKeyEvent(hs.keycodes.map.shift, false):post()
+--         end
+--         os.execute("sleep " .. tonumber(.2))
+--    end
+-- end)
 
 -- send ctrl+f2 to focus the menubar when it is hidden
 -- 2024/08/09: doesnt seem to be working, using shift+cmd+? to show menu bar
