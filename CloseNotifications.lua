dc436ff4afaea8a49e4b53ad4dd09e1e3b95e1bb
Evan Johnson <ev.jonnson@gmail.com>
2024-11-03 11:21:59 -0700

    update close notifications script for macOS 15.1

--- CloseNotifications.lua
+++ CloseNotifications.lua
@@ -1,6 +1,6 @@
 -- Close all visible notifications in Notification Center.
 hs.hotkey.bind({ "ctrl", "cmd", "alt" }, "x", function()
     hs.osascript.applescriptFromFile(
-        os.getenv("HOME") .. "/.hammerspoon/scripts/CloseNotifications.applescript"
+        os.getenv("HOME") .. "/.hammerspoon/scripts/CloseNotifications-15.1.applescript"
     )
 end)
